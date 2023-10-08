52c3ab71da18a925949cd084f87cda1c0a2e4c8a
Filip Waszak <254925@student.pwr.edu.pl>
2023-10-08 22:52:14 +0200

    data

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-import argparse
-import requests
-from torchvision.transforms import ToTensor, Lambda
-from pathlib import Path
+# import argparse
+# import requests
+# from torchvision.transforms import Lambda
+# from pathlib import Path
+# from configs.config_utils import CONFIG
 import pandas as pd
 from CustomDataset import CustomImageDataset
-# from configs.config_utils import CONFIG
 from myfunctions import print_image
 from torchvision import transforms
 
-
-# batch_size = 16
-# img_width, img_height, img_channels = 28,28,1
-# no_classes = 4
 
 # def parse_args():
 #     """User-friendly command lines"""
@@ -23,31 +19,22 @@ from torchvision import transforms
 
 def main():
 
-    # input_path = '../input'
-    # url = "https://example.com/image.jpg"
-    # response = requests.get(url)
-    # with open("image.jpg", "wb") as f:
-    #     f.write(response.content)
-
     data_transform = transforms.Compose([
                         transforms.Resize(size=(256, 256)),
-                        transforms.RandomVertical Flip(p=0.5),
+                        transforms.RandomVerticalFlip(p=0.5),
                         transforms.ToTensor()])
 
     ds = CustomImageDataset(
         annotations_file=pd.read_csv('data/data.csv', sep=';'),
         img_dir="data/images/",
         transform=data_transform,
-        target_transform= None #Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
+        target_transform=None
+        # Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
     )
 
     img, label = ds.__getitem__(4)
     print_image(img)
 
-    # parser = parse_args()
-    # cfg = CONFIG(parser)
-    # cfg.log_string('Loading configurations.')
-    # cfg.log_string(cfg.config)
 
 if __name__ == '__main__':
     main()
--- myfunctions.py
+++ myfunctions.py
@@ -1,8 +1,23 @@
 import matplotlib.pyplot as plt
 from torch import permute
+from time import perf_counter
+
 
 def print_image(img):
-    img = permute(img, (1,2,0))
+    img = permute(img, (1, 2, 0))
     plt.figure(figsize=(6, 6))
     plt.imshow(img)
     plt.show()
+
+
+def get_time(func):
+    def wrapper(*args, **kwargs):
+
+        start = perf_counter()
+        func(*args, **kwargs)
+        end = perf_counter()
+        total_time = round(end - start,2)
+
+        print(f"time: {total_time:.2f} seconds")
+
+    return wrapper
