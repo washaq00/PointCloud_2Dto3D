a2a7fa00c1337ea245346689d39276ab823c62ca
Filip Waszak <254925@student.pwr.edu.pl>
2023-10-15 00:02:39 +0200

    s

--- CustomDataset.py
+++ CustomDataset.py
@@ -1,26 +1,41 @@
 import os
-import pandas as pd
 from PIL import Image
 from torch.utils.data import Dataset
+import pathlib
+from typing import Tuple, Dict, List #typical list is not
+
+
+def find_classes(dir: str) -> Tuple[List[str],Dict[str, int]]:
+    #get the class names with scanning directory (it checkes if there are any subfolder)
+    classes = sorted(entry.name for entry in os.scandir(dir) if entry.is_dir())
+
+    #show the error
+    if not classes:
+        raise FileNotFoundError(f"Couldn't find any classes")
+
+    #creating a Dict with name and id
+    classes_to_id = {cls_name: i for i, cls_name in enumerate(classes)}
+    return classes, classes_to_id
 
 
 class CustomImageDataset(Dataset):
-    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = annotations_file
-        self.img_dir = img_dir
+    def __init__(self, img_dir, transform =None):
+        self.classes, self.classes_idx = find_classes(img_dir)
+        self.paths = list(pathlib.Path(img_dir).glob("*/*.png"))
         self.transform = transform
-        self.target_transform = target_transform
+
+    def load_image(self, index: int) -> Image.Image:
+        image_path = self.paths[index]
+        return Image.open(image_path)
 
     def __len__(self) -> int:
-        return len(self.img_labels)
+        return len(self.paths)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = Image.open(img_path)
-        label = self.img_labels.iloc[idx, 1]
+        image = self.load_image(idx)
+        class_name = self.paths[idx].parent.name
+        class_idx = self.classes_idx[class_name]
         if self.transform:
             image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
+        return image, class_idx
 
--- main.py
+++ main.py
@@ -3,10 +3,10 @@
 # from torchvision.transforms import Lambda
 # from pathlib import Path
 # from configs.config_utils import CONFIG
-import pandas as pd
-from CustomDataset import CustomImageDataset
-from myfunctions import print_image
+import os
+from CustomDataset import find_classes, CustomImageDataset
 from torchvision import transforms
+from torch.utils.data import DataLoader
 
 
 # def parse_args():
@@ -19,21 +19,51 @@ from torchvision import transforms
 
 def main():
 
-    data_transform = transforms.Compose([
-                        transforms.Resize(size=(256, 256)),
-                        transforms.RandomVerticalFlip(p=0.5),
-                        transforms.ToTensor()])
+    # if Path("helper_functions.py").is_file():
+    #   print("helper_functions.py already exists, skipping download")
+    # else:
+    #   request = requests.get("https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py")
+    #   with open("helper_functions.py", "wb") as f:
+    #     f.write(request.content)
 
-    ds = CustomImageDataset(
-        annotations_file=pd.read_csv('data/data.csv', sep=';'),
-        img_dir="data/images/",
-        transform=data_transform,
-        target_transform=None
-        # Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(0, torch.tensor(y), value=1))
-    )
+    BATCH_SIZE = 32
+    NUM_WORKERS = os.cpu_count()
 
-    img, label = ds.__getitem__(4)
-    print_image(img)
+    # implement Data Augmentation and Image Resizing
+    train_transform = transforms.Compose([
+        transforms.Resize(size=(256, 256)),
+        transforms.TrivialAugmentWide(num_magnitude_bins=31),
+        transforms.ToTensor()])
+
+    test_transform = transforms.Compose([
+        transforms.Resize(size=(256, 256)),
+        transforms.ToTensor()])
+
+    train_dir = os.path.join(os.getcwd(), "data/train/")
+    # print(find_classes(train_dir))
+    test_dir = os.path.join(os.getcwd(), "data/test/")
+
+    train_data = CustomImageDataset(img_dir=train_dir,  # target folder of images
+                                    transform=train_transform)  # transforms to perform on labels (if necessary)
+
+    test_data = CustomImageDataset(img_dir=test_dir,
+                                   transform=test_transform)
+
+    print(f"test data {train_data}")
+    class_names = train_data.classes
+    # print(class_names)
+    # img, label = train_data[0][0], train_data[0][1]
+    # print_image(img)
+
+    train_dataloader = DataLoader(dataset=train_data,
+                                  batch_size=BATCH_SIZE,
+                                  num_workers=NUM_WORKERS,
+                                  shuffle=True)
+
+    test_dataloader = DataLoader(dataset=test_data,
+                                 batch_size=BATCH_SIZE,
+                                 num_workers=NUM_WORKERS,
+                                 shuffle=False)  # don't usually need to shuffle testing data
 
 
 if __name__ == '__main__':
